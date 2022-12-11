b62986ba2105403185aee9e1a4ca13b6b4477e13
casio25 <mishakolomiets355@gmail.com>
2022-12-11 09:07:18 +0200

    added apixu api

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,33 @@
+/* eslint-disable max-len */
 const functions = require("firebase-functions");
-
+const apixu = require("apixu");
 
 const {Telegraf} = require("telegraf");
 
+
+const apixuClient = new apixu.Apixu({
+  apikey: "f7a45f30f0ea53252878faca58e0ab21",
+});
+
 const bot = new Telegraf("5692567752:AAGGr9GRHzAlsxAFjJoDACIr8mZTTyLT8O4");
 bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.on("text", (ctx) => {
+  const query = ctx.update.message.text;
+  apixuClient.current(query).then((current) => {
+    return ctx.reply(
+        `The current weather in ${query} is C: ${current.current.temp_c} F:${current.current.temp_f}`);
+  }).catch((err) => {
+    return ctx.reply("This city is not exists", err);
+  });
+});
 bot.launch();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
+
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  apixuClient.current("London").then((current) => {
+    return response.send(current);
+  }).catch((err) => {
+    return response.send(err);
+  });
 });
