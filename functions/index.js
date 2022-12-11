c6481089f6b7aac56515bc18a912e7b586bb5c25
casio25 <mishakolomiets355@gmail.com>
2022-12-11 07:29:28 +0200

    added telegraf and tested our telegramBot

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require("firebase-functions");
 
+
+const {Telegraf} = require("telegraf");
+
+const bot = new Telegraf("5692567752:AAGGr9GRHzAlsxAFjJoDACIr8mZTTyLT8O4");
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.launch();
+
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
